1dce3a340d56af8f1ad5fbee96332eaff05023bb
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-17 10:27:12 -0500

    rendering beasts data dynamically

--- class01/lab01/src/Main.js
+++ class01/lab01/src/Main.js
@@ -1,11 +1,15 @@
 import HornedBeast from './HornedBeast';
+import hornedBeasts from './data';
 
 function Main() {
+    let myBeasts = [];
+    hornedBeasts.forEach(hornedBeast => {
+        myBeasts.push(<HornedBeast name={hornedBeast.name} title={hornedBeast.title} imgUrl={hornedBeast.image_url} description={hornedBeast.description} />)
+    })
     return (
-        <>
-        <HornedBeast title="this is a title" imageUrl="" description="this is a description"/>
-        <HornedBeast title="unicorn" imageUrl="" description="a unicorn and narwhal nuzzling their horns"/>
-        </>
+        <main>
+           {myBeasts} 
+        </main>
     )
 }
 
