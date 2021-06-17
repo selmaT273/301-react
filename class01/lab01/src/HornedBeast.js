f42b53f14b50c65a9450ab07a265356de53b56ee
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-17 14:31:29 -0500

    began implementing useState to keep track of how many times someone clicks on an image by updating the number counter for now

--- class01/lab01/src/HornedBeast.js
+++ class01/lab01/src/HornedBeast.js
@@ -1,9 +1,14 @@
+import React, { useState } from 'react';
+
 export default function HornedBeast(props) {
+    const [count, setCount] = useState(0);
+
     return (
         <>
         <h2>{props.title}</h2>
-        <img title={props.name} alt={props.name + "image"} src={props.imgUrl} />
+        <img onClick={() => setCount(count + 1)} title={props.name} alt={props.name + "image"} src={props.imgUrl} />
         <p>{props.description}</p>
+        <p>You voted for this {count} times</p>
         </>
     );
 }
