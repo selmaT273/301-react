c16d17272f2280315345f164e3ad1679171f178f
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-21 15:21:31 -0500

    converted data file to json file type, refactored state so that it is captured in the app and passed down to main

--- class01/lab01/src/App.js
+++ class01/lab01/src/App.js
@@ -3,13 +3,23 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
-
+import data from './data.json';
+import HornedBeast from './HornedBeast'
 class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      hornedBeastsData: data
+    }
+  }
+
   render() {
     return (
       <>
       <Header />
-      <Main />
+      <Main data = {this.state.hornedBeastsData}/>
+      <HornedBeast />
       <Footer />
     </>
     );
--- class01/lab01/src/Main.js
+++ class01/lab01/src/Main.js
@@ -1,18 +1,21 @@
 import HornedBeast from './HornedBeast';
-import hornedBeasts from './data';
 import CardColumns from 'react-bootstrap/CardColumns';
 import './main.css';
 import React from 'react';
 
 class Main extends React.Component {
     render() {
-        let myBeasts = [];
-        hornedBeasts.forEach(hornedBeast => {
-            myBeasts.push(<HornedBeast key={hornedBeast.image_url} name={hornedBeast.name} title={hornedBeast.title} imgUrl={hornedBeast.image_url} description={hornedBeast.description} />)
-        })
         return (
             <>
-            <CardColumns className="card-columns">{myBeasts}</CardColumns>
+            <CardColumns className="card-columns">
+                {this.props.data.map((hornedBeast) => (<HornedBeast 
+                    key={hornedBeast.image_url} 
+                    name={hornedBeast.name} 
+                    title={hornedBeast.title} 
+                    imgUrl={hornedBeast.image_url} 
+                    description={hornedBeast.description} />)
+                )}
+            </CardColumns>
             </>
         )
     }
