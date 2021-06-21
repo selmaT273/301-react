0e1201aaca2f5b49aef674f45d700e01f61d7d67
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-21 15:29:33 -0500

    renamed my json data so to clarify the distinction between different variables

--- class01/lab01/src/App.js
+++ class01/lab01/src/App.js
@@ -3,14 +3,14 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
-import data from './data.json';
+import beastData from './data.json';
 import HornedBeast from './HornedBeast'
 class App extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {
-      hornedBeastsData: data
+      hornedBeastsData: beastData
     }
   }
 
