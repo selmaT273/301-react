4932b671cdc6db6ca97e29f220ebdb0dfce22539
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-22 15:12:26 -0500

    cleaned up console logs

--- class03/demo/src/Child.js
+++ class03/demo/src/Child.js
@@ -3,15 +3,13 @@ import React from 'react';
 class Child extends React.Component {
 
     //need an event handler to run when child asks for a macaron
-    //child can send info up to the parent by using the function the parent passed down
     askForMacaron = () => {
-        console.log('ask for a macaron');
+        //child can send info up to the parent by using the function the parent passed down
         this.props.handleMacarons();
     }
 
     render(){
         // using this.props allows us to access the props(in this case, an object) that we sent over from the parent
-        console.log('child props', this.props);
         return(
             <>
             <h3>I am the child and I have {this.props.aesonTotal} macarons now.</h3>
