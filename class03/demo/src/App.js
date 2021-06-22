69fa2d572d800c1a1d3f392f6249e30dbd33a093
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-22 15:49:56 -0500

    added images to demo

--- class03/demo/src/App.js
+++ class03/demo/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Parent from './Parent';
-
+import Card from 'react-bootstrap/Card';
 class App extends React.Component {
   render(){
     return(
       <>
       <h1>This is App</h1>
+      <Card style={{ display: 'inline-block' }}>
+        <Card.Img variant="top" src="http://placehold.it/500x100/444" />
+      </Card>
       <Parent />
       </>
     )
--- class03/demo/src/Child.js
+++ class03/demo/src/Child.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import data from './data.json';
 class Child extends React.Component {
 
     //need an event handler to run when child asks for a macaron
@@ -9,11 +9,23 @@ class Child extends React.Component {
     }
 
     render(){
+        let animalPics = data;
+        console.log(animalPics);
+        let picElements = animalPics.map((pic, index) => (
+            <img
+                src={pic}
+                alt="horns"
+                title="horns"
+                width={150}
+                key={index}
+            />
+        ))
         // using this.props allows us to access the props(in this case, an object) that we sent over from the parent
         return(
             <>
             <h3>I am the child and I have {this.props.aesonTotal} macarons now.</h3>
             <button onClick={this.askForMacaron}>Ask for a macaron</button>
+            {picElements}
             </>
             // we can use this.EventHandler to refer to this instance of child that will be created
         );
