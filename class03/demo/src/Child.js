e6f14b13450f5dce6128d9c7195c1503e16950d0
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-22 14:48:26 -0500

    added event handler to run when button is clicked, but just console logs for now

--- class03/demo/src/Child.js
+++ class03/demo/src/Child.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 class Child extends React.Component {
+
+    //need an event handler to run when child asks for a macaron
+    giveMacaron = () => {
+        console.log('give a macaron');
+    }
+
     render(){
         // using this.props allows us to access the props(in this case, an object) that we sent over from the parent
         console.log('child props', this.props);
         return(
             <>
             <h3>I am the child and I have {this.props.aMacarons} macarons now.</h3>
+            <button onClick={this.giveMacaron}>Ask for a macaron</button>
             </>
+            // we can use this.EventHandler to refer to this instance of child that will be created
         );
     }
 }
