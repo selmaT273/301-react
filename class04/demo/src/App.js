b2cebeabca5eaf92cf5ac716bddfe40f916ccfea
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-22 20:16:37 -0500

    demo app created and proof of life

---
+++ class04/demo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render(){
+    return(
+      <>
+      <h1>proof of life</h1>
+      </>
+    )
+  }
+}
+
+export default App;
