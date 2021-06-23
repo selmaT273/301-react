72212c8bed8c80915756be2703dd8b3ded91fdf7
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-22 20:47:15 -0500

    created basic form and used setstate to capture the name typed and setting to username

--- class04/demo/src/App.js
+++ class04/demo/src/App.js
@@ -3,10 +3,37 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: ''
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('submitted');
+  }
+
+  handleUsername = e => {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
   render(){
     return(
       <>
-      <h1>proof of life</h1>
+      <h1>Hello {this.state.username}</h1>
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input id="name" name="name" type="text" onInput={this.handleUsername}/>
+        <br />
+        <label htmlFor="dob">Date of Birth</label>
+        <input type="date" name="dob" id="dob" />
+        <br />
+        <input type="submit" />
+      </form>
       </>
     )
   }
