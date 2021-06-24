03bbdca7158ac8d408020c818263a2f42aed7546
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-24 12:15:13 -0500

    cleaned up console logs andcreated state to capture title and director and update state when fetching

--- class06/demo06/src/App.js
+++ class06/demo06/src/App.js
@@ -3,22 +3,32 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      title: '',
+      director: ''
+    }
+  }
 
   handleFetch = async() => {
     console.log('that is so fetch!');
     // await resolves the promise that axios returns, so that we can assign it to a variable
     let data = await axios.get('https://swapi.dev/api/people/1/');
-    console.log(data);
     // practicing accessing the films inside of the data object
     let filmData = await axios.get(data.data.films[0]);
-    console.log(filmData);
+    this.setState({
+      title: filmData.data.title,
+      director: filmData.data.director
+    })
   }
 
   render(){
-
     return(
       <>
       <h1 onClick={this.handleFetch}>hellllllloooo</h1>
+      <h2>The movie {this.state.title} was directed by {this.state.director}</h2>
       </>
     )
   }
