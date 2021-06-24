5b21d6036a87a1e8e63361e3ca3bc32b73931550
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-24 12:02:09 -0500

    accessing film array inside luke skywalker object

--- class06/demo06/src/App.js
+++ class06/demo06/src/App.js
@@ -9,6 +9,9 @@ class App extends React.Component {
     // await resolves the promise that axios returns, so that we can assign it to a variable
     let data = await axios.get('https://swapi.dev/api/people/1/');
     console.log(data);
+    // practicing accessing the films inside of the data object
+    let filmData = await axios.get(data.data.films[0]);
+    console.log(filmData);
   }
 
   render(){
