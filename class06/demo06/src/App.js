58f7f070a013df35d28f3e10b69767b0100f1ac8
Stacey Teltser <stacey.teltser@gmail.com>
2021-06-24 11:35:48 -0500

    demo scaffolded

---
+++ class06/demo06/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render(){
+    return(
+      <>
+      <h1>hellllllloooo</h1>
+      </>
+    )
+  }
+}
+
+export default App;
